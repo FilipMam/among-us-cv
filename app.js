111558f35ff455662cc01baae23c63c72b1017c8
filipMaM <filip.mamcarczyk@gmail.com>
2020-12-30 10:06:43 +0100

    Add stars, trunk and enginges

--- app.js
+++ app.js
@@ -49,4 +49,18 @@
       crewmate.stopMoving(keyMap[event.keyCode]);
     }
   });
+
+  // render stars
+  const starContainer = document.querySelector("#stars"); 
+  for (let i=0; i<100; i++) {
+    const randomNumber = Math.random();
+    const translateX = randomNumber*100;
+    const delay = i*1/50;
+    const starElement = document.createElement("span");
+    starElement.classList.add("star");
+    starElement.style.left = `${translateX}%`;
+    starElement.style.animationDelay = `${delay}s`;
+    starContainer.appendChild(starElement);
+  }
+  
 })()
